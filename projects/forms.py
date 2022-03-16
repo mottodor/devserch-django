840ccb6ab184507d7e60434990d297e1058be499
Kantemir Shogenov <kantemir-83@yandex.ru>
2022-03-16 21:05:33 +0300

    Update projects form.py to add classes

--- projects/forms.py
+++ projects/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Project
 
 
@@ -7,3 +8,11 @@ class ProjectForm(ModelForm):
         model = Project
         fields = ['title', 'description', 'feature_image', 'demo_link',
                   'source_link', 'tags']
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple()
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({'class': 'input'})
