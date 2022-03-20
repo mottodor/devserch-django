d786983c93f10ef38b6c0b25554c0d9e50fb2304
Kantemir Shogenov <kantemir-83@yandex.ru>
2022-03-20 11:31:47 +0300

    Fix alert style

--- static/uikit/app.js
+++ static/uikit/app.js
@@ -1,8 +1,3 @@
-// Invoke Functions Call on Document Loaded
-document.addEventListener('DOMContentLoaded', function () {
-  hljs.highlightAll();
-});
-
 let alertWrapper = document.querySelector('.alert')
 let alertButton = document.querySelector('.alert__close')
 
